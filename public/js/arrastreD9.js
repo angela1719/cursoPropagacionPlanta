d58eed9be1201cbc6140ee677e73ffaf1f193d53
angela1719 <angelarm783@gmail.com>
2023-11-21 11:51:24 -0500

    version cuadros punteados para apareamiento y mensaje emergente al posicionarlos

--- public/js/arrastreD9.js
+++ public/js/arrastreD9.js
@@ -1,52 +1,62 @@
+// archivo: public/js/arrastreD9.js
 console.log("¡Mi JavaScript está vinculado correctamente!");
-window.addEventListener('load', () => {
 
-    // Obtener todos los elementos con la clase cajaPalabra
+window.addEventListener('load', () => {
     const cajasPalabra = document.querySelectorAll(".cajaPalabra");
-    
-    let isDragging = false;
-    let offsetX, offsetY, draggedElement;
-    
-    // Agregar eventos de mousedown a cada elemento cajaPalabra
-    cajasPalabra.forEach(caja => {
-      caja.addEventListener("mousedown", function (event) {
-        isDragging = true;
-        draggedElement = this;
-        offsetX = event.clientX - this.getBoundingClientRect().left;
-        offsetY = event.clientY - this.getBoundingClientRect().top;
-        // Cambiar el estilo para indicar que el elemento está siendo arrastrado
-        this.style.opacity = "0.5";
-      });
+    const dropZones = document.querySelectorAll(".drop-zone");
+
+    cajasPalabra.forEach((caja, index) => {
+        let isDragging = false;
+        let offsetX, offsetY, draggedElement;
+
+        caja.addEventListener("mousedown", function (event) {
+            isDragging = true;
+            draggedElement = this;
+            offsetX = event.clientX - this.getBoundingClientRect().left;
+            offsetY = event.clientY - this.getBoundingClientRect().top;
+            this.style.opacity = "0.5";
+        });
+
+        document.addEventListener("mousemove", function (event) {
+            if (isDragging && draggedElement) {
+                draggedElement.style.left = event.clientX - offsetX + "px";
+                draggedElement.style.top = event.clientY - offsetY + "px";
+            }
+        });
+
+        document.addEventListener("mouseup", function () {
+            if (isDragging && draggedElement) {
+                draggedElement.style.opacity = "1";
+                isDragging = false;
+
+                const dropZone = findDropZone(event.clientX, event.clientY);
+
+                if (dropZone && !dropZone.hasChildNodes()) {
+                    const palabra = draggedElement.innerText;
+                    dropZone.innerText = `¡${palabra} colocada correctamente!`;
+                } else {
+                    // La palabra no está en un drop-zone válido, resetea su posición
+                    draggedElement.style.left = "";
+                    draggedElement.style.top = "";
+                }
+
+                draggedElement = null;
+            }
+        });
     });
-    
-    // Agregar evento de mousemove al documento para seguir el arrastre
-    document.addEventListener("mousemove", function (event) {
-      if (isDragging && draggedElement) {
-        draggedElement.style.left = event.clientX - offsetX + "px";
-        draggedElement.style.top = event.clientY - offsetY + "px";
-      }
-    });
-    
-    // Agregar evento de mouseup al documento para finalizar el arrastre
-    document.addEventListener("mouseup", function () {
-      if (isDragging && draggedElement) {
-        // Restaurar el estilo original
-        draggedElement.style.opacity = "1";
-        isDragging = false;
-        draggedElement = null;
-    
-        // Comprobar si el elemento está sobre la estructura anatómica correspondiente
-        const estructuraAnatomica = document.querySelector(".cuadroA");
-        if (
-          event.clientX > estructuraAnatomica.getBoundingClientRect().left &&
-          event.clientX < estructuraAnatomica.getBoundingClientRect().right &&
-          event.clientY > estructuraAnatomica.getBoundingClientRect().top &&
-          event.clientY < estructuraAnatomica.getBoundingClientRect().bottom
-        ) {
-          // Si el elemento se soltó en la estructuraAnatomica, realiza acciones adicionales aquí
-          alert("¡Palabra colocada correctamente!");
+
+    function findDropZone(x, y) {
+        for (const dropZone of dropZones) {
+            const dropZoneRect = dropZone.getBoundingClientRect();
+            if (
+                x > dropZoneRect.left &&
+                x < dropZoneRect.right &&
+                y > dropZoneRect.top &&
+                y < dropZoneRect.bottom
+            ) {
+                return dropZone;
+            }
         }
-      }
-    });
-    
-})
+        return null;
+    }
+});
